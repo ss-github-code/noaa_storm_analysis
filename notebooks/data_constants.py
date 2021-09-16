502ab51def44b689baa254c6c6f74a4ebc08a640
shiv saxena <shivsaxena@hotmail.com>
2021-09-16 08:14:48 -0700

    save a copy of the database

--- notebooks/data_constants.py
+++ notebooks/data_constants.py
@@ -4,6 +4,7 @@ BEGIN_YEAR = 2000
 END_YEAR = 2022
 
 ZONE_COUNTY_CORR_URL = 'https://www.weather.gov/source/gis/Shapefiles/County/bp10nv20.dbx'
+ZONE_COUNTY_CORR_CSV = 'https://raw.githubusercontent.com/ss-github-code/noaa_storm_analysis/main/data/zone_county_corr.csv'
 NOAA_CSVFILES_URL = 'https://www.ncei.noaa.gov/pub/data/swdi/stormevents/csvfiles/'
 GEOJSON_COUNTIES_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
 
