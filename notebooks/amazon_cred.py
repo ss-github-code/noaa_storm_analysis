4591028b37dd0c512ab3b0fc7987c19542efd2c9
Shiv Saxena <shivsaxena@hotmail.com>
2021-09-17 10:43:13 -0700

    password changed after deploy

--- notebooks/amazon_cred.py
+++ notebooks/amazon_cred.py
@@ -2,5 +2,5 @@
 ENDPOINT = 'noaa-db-inst.ca5kgcmut2mu.us-east-1.rds.amazonaws.com'
 PORT = 5432
 USER = 'postgresql'
-PASSWORD = '***REMOVED***' # changed!!
+PASSWORD = 'PASSWORD' # changed!!
 DATABASE = 'noaapostgres'
