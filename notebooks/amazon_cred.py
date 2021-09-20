e25f666f13f56f3aa03c954ba416a0ff84f06d99
Shiv Saxena <shivsaxena@hotmail.com>
2021-09-20 01:31:29 -0700

    storm analysis updated

--- notebooks/amazon_cred.py
+++ notebooks/amazon_cred.py
@@ -2,5 +2,5 @@
 ENDPOINT = 'noaa-db-inst.ca5kgcmut2mu.us-east-1.rds.amazonaws.com'
 PORT = 5432
 USER = 'postgresql'
-PASSWORD = 'PASSWORD' # changed!!
+PASSWORD = '***REMOVED***' # changed!!
 DATABASE = 'noaapostgres'
